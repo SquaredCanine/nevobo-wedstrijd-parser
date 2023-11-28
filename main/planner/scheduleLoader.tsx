5fe93869c4b2d51d4bedbde6fdf5e62498ca30c9
Quinten Stekelenburg <quinten.stekelenburg@capgemini.com>
2023-11-28 14:25:42 +0100

    Minor excel export style changes

--- main/planner/scheduleLoader.tsx
+++ main/planner/scheduleLoader.tsx
@@ -81,11 +81,13 @@ export class ScheduleLoader {
                 ["", `${key} Competitie`], //Date header
                 entryHeader //Column header
             ]
-            matches.forEach((indeling) => {
+            matches.forEach((indeling: PlannedMatch) => {
                 newData.push(
                     [
+                        "",
                         key,
                         indeling.wedstrijd.tijd,
+                        indeling.wedstrijd.veld,
                         indeling.wedstrijd.thuisTeam,
                         indeling.wedstrijd.uitTeam,
                         "COACH",
@@ -99,13 +101,27 @@ export class ScheduleLoader {
         const worksheet = XLSX.utils.json_to_sheet(
             data
             , { skipHeader: true })
-            worksheet['!merges'] = []
-            worksheet['!merges'].push(...rowsToExtend.map((r) => {
+        worksheet['!merges'] = []
+        worksheet['!merges'].push(...rowsToExtend.map((r) => {
             return {
                 s: { r, c: 1 }, // s ("start"): B
                 e: { r, c: 8 }  // e ("end"): I
-            }   
+            }
         }))
+        const constantFactor = 3;
+        worksheet['!cols'] = []
+        worksheet['!cols'][0] = { wpx: 30 * constantFactor } //Links
+        worksheet['!cols'][1] = { wpx: 30 * constantFactor } //Datum
+        worksheet['!cols'][2] = { wpx: 15 * constantFactor } //Tijd
+        worksheet['!cols'][3] = { wpx: 15 * constantFactor } //Veld
+        worksheet['!cols'][4] = { wpx: 165 } //Team thuis
+        worksheet['!cols'][5] = { wpx: 165 } //Team uit
+        worksheet['!cols'][6] = { wpx: 45 * constantFactor } //Coach
+        worksheet['!cols'][7] = { wpx: 45 * constantFactor } //Scheids
+        worksheet['!cols'][8] = { wpx: 30 * constantFactor } //Teller
+        worksheet['!cols'][9] = { wpx: 15 * constantFactor } //Rechts
+        worksheet['!rows'] = []
+        worksheet['!rows'][0] = { hpt: 150 }
         const workbook = XLSX.utils.book_new();
         XLSX.utils.book_append_sheet(workbook, worksheet, "Schema");
         XLSX.writeFile(workbook, file, { compression: true });
